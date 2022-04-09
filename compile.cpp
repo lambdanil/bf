19d1bf1017d7c3627223b54acf517fb4d671689c
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-04-09 19:26:31 +0200

    Add files via upload

--- compile.cpp
+++ compile.cpp
@@ -3,66 +3,51 @@
 
 using std::cout;
 using std::cin;
+using std::string;
 
-void evaluate(char* cmd) {
-    char* ptrMem;
-    ptrMem = new char[30000]; // Allocate memory for program, 30000 bytes currently, maybe a vector would be better for dynamic memory size?
-    char* ptr;
-    ptr = ptrMem;
+void evaluate(char* cmd, string* output) {
+    *output += "#include<iostream>\n";
+    *output += "int main() {\n";
+    *output += "char* ptrMem;\n";
+    *output += "ptrMem = new char[30000];\n";
+    *output += "char* ptr;\n";
+    *output += "ptr = ptrMem;\n";
     char* cins;
-    bool inLoop = false;
     cins = cmd;
     ptrdiff_t bytes = cins-cmd;
     int cmdlen = strlen(cmd);
     while (true) { 
-        if (inLoop == false) { // Skips calculation if in a loop, small speed improvement
-            bytes = cins-cmd; // Calculate memory between current instruction and last instruction
-            if (bytes > cmdlen-1) break; // Probably faster than comparing memory between last instruction and current instruction
-        }
+        bytes = cins-cmd; // Calculate memory between current instruction and last instruction
+        if (bytes > cmdlen-1) break; // Probably faster than comparing memory between last instruction and current instruction
         switch (*cins) {
             case '>':
-                ++ptr;
+                *output += "++ptr;\n";
                 break;
             case '<':
-                --ptr;
+                *output += "--ptr;\n";
                 break;
             case '+':
-                ++(*ptr);
+                *output += "++(*ptr);\n";
                 break;
             case '-':
-                --(*ptr);
+                *output += "--(*ptr);\n";
                 break;
             case '.':
-                cout << *ptr;
+                *output += "std::cout << *ptr;\n";
                 break;
             case ',':
-                cin >> *ptr;
+                *output += "std::cin >> *ptr;\n";
                 break;
             case '[':
-                inLoop = true;
-                if (*ptr == 0) {
-                    inLoop = false;
-                    int depth = 1;
-                    while (depth != 0) {
-                        ++cins;
-                        if (*cins == '[') ++depth;
-                        else if (*cins == ']') --depth;
-                    }
-                }
+                *output += "while (*ptr) {\n";
                 break;
             case ']':
-                int depth = 1;
-                while (depth != 0) {
-                    --cins;
-                    if (*cins == ']') ++depth;
-                    else if (*cins == '[') --depth;
-                }
-                --cins;
+                *output += "}\n";
                 break;
         };
     ++cins;
     }
-    delete ptrMem;
+    *output += "};\n";
 }
 
 int main(int argc, char** argv) {
@@ -72,6 +57,8 @@ int main(int argc, char** argv) {
     int index = 0;
     bool newline = true;
     bool isComment = false;
+    string* output;
+    output = new string;
     int i = 0;
     for (int i = 0; i < strlen(argv[1]); i++) {
         if (argv[1][i] == '%') newline = false; // Handle comments
@@ -86,6 +73,8 @@ int main(int argc, char** argv) {
             index++;
         }
     }
-    evaluate(cmd); // Call evaluation
+    evaluate(cmd, output); // Call evaluation
+    cout << *output;
     delete[] cmd;
+    delete output;
 }
