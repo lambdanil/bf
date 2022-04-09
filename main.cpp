74b9413b9dd5a6ae68bf6c519df68239b2bd6cde
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-04-09 13:52:08 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string.h>
+
+using std::cout;
+using std::cin;
+
+void evaluate(char* cmd) {
+    char* ptrMem;
+    ptrMem = new char[30000];
+    char* ptr;
+    ptr = ptrMem;
+    char* cins;
+    cins = cmd;
+    ptrdiff_t bytes = cins-cmd;
+    int cmdlen = strlen(cmd);
+    while (true) { 
+        bytes = cins-cmd;
+        if (bytes > cmdlen-1) break;
+        switch (*cins) {
+            case '>':
+                ++ptr;
+                break;
+            case '<':
+                --ptr;
+                break;
+            case '+':
+                ++(*ptr);
+                break;
+            case '-':
+                --(*ptr);
+                break;
+            case '.':
+                cout << *ptr;
+                break;
+            case ',':
+                cin >> *ptr;
+                break;
+            case '[':
+                if (*ptr == 0) {
+                    int depth = 1;
+                    while (depth != 0) {
+                        ++cins;
+                        if (*cins == '[') ++depth;
+                        else if (*cins == ']') --depth;
+                    }
+                }
+                break;
+            case ']':
+                int depth = 1;
+                while (depth != 0) {
+                    --cins;
+                    if (*cins == ']') ++depth;
+                    else if (*cins == '[') --depth;
+                }
+                --cins;
+                break;
+        };
+    ++cins;
+    }
+    delete ptrMem;
+}
+
+int main(int argc, char** argv) {
+    char* cmd;
+    cmd = argv[1];
+    evaluate(cmd);
+}
