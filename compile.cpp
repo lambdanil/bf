65a424459e4fc2cb3a564e9db51cc418b48dda8c
Jan Novotný <59669705+CuBeRJAN@users.noreply.github.com>
2022-04-09 19:38:28 +0200

    Add files via upload

--- compile.cpp
+++ compile.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<string.h>
+#include<vector>
+#include<fstream>
 
 using std::cout;
 using std::cin;
@@ -51,8 +53,17 @@ void evaluate(char* cmd, string* output) {
 }
 
 int main(int argc, char** argv) {
+    std::ifstream file(argv[1], std::ios::binary | std::ios::ate);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<char> buffer(size);
+    if (file.read(buffer.data(), size))
+    {
+        /* worked! */
+    
     char* cmd;
-    cmd = new char[100000];
+    cmd = new char[5000000];
     //cmd = argv[1];
     int index = 0;
     bool newline = true;
@@ -60,21 +71,21 @@ int main(int argc, char** argv) {
     string* output;
     output = new string;
     int i = 0;
-    for (int i = 0; i < strlen(argv[1]); i++) {
-        if (argv[1][i] == '%') newline = false; // Handle comments
-        if (argv[1][i] == '\n') newline = true;
-        if (argv[1][i] == '#') {
+    for (int i = 0; i < buffer.size(); i++) {
+        if (buffer.at(i) == '%') newline = false; // Handle comments
+        if (buffer.at(i) == '\n') newline = true;
+        if (buffer.at(i) == '#') {
             if (isComment) isComment=false;
             else isComment=true;
         }
         // This parses the instructions into cmd array
-        if ((argv[1][i] == '>' || argv[1][i] == '<' || argv[1][i] == '+' || argv[1][i] == '-' || argv[1][i] == '[' || argv[1][i] == ']' || argv[1][i] == ',' || argv[1][i] == '.') && newline && isComment == false) {
-            cmd[index] = argv[1][i];
+        if ((buffer.at(i) == '>' || buffer.at(i) == '<' || buffer.at(i) == '+' || buffer.at(i) == '-' || buffer.at(i) == '[' || buffer.at(i) == ']' || buffer.at(i) == ',' || buffer.at(i) == '.') && newline && isComment == false) {
+            cmd[index] = buffer.at(i);
             index++;
         }
     }
     evaluate(cmd, output); // Call evaluation
     cout << *output;
     delete[] cmd;
-    delete output;
+    delete output;}
 }
